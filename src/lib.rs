fa31757b462a17597deb5776186eaeda44b62ef0
Nick Fitzgerald <fitzgen@gmail.com>
2018-05-22 13:08:57 -0700

    Add a small example of using wasm-bindgen to the crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
+//! My awesome Rust and WebAssembly project!
+
+#![feature(proc_macro, wasm_custom_section)]
+
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+/// My publicly exported type!
+#[wasm_bindgen]
+pub struct Counter {
+    count: u32
+}
+
+/// My publicly exported methods!
+#[wasm_bindgen]
+impl Counter {
+    pub fn new() -> Counter {
+        Counter {
+            count: 0
+        }
+    }
+
+    pub fn increment(&mut self) -> u32 {
+        let x = self.count;
+        self.count += 1;
+        x
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn tests_inside_the_crate_work() {
         assert_eq!(2 + 2, 4);
     }
 }
