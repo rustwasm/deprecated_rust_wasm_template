d954e5950c03b9e8a1b361b6e659ea29e7f45864
Nick Fitzgerald <fitzgen@gmail.com>
2018-05-22 16:16:06 -0700

    Run `cargo fmt`

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,14 @@ use wasm_bindgen::prelude::*;
 /// My publicly exported type!
 #[wasm_bindgen]
 pub struct Counter {
-    count: u32
+    count: u32,
 }
 
 /// My publicly exported methods!
 #[wasm_bindgen]
 impl Counter {
     pub fn new() -> Counter {
-        Counter {
-            count: 0
-        }
+        Counter { count: 0 }
     }
 
     pub fn increment(&mut self) -> u32 {
